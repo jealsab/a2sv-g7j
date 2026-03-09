5dc4e0af437af75c46337a9c656a79ea431cd3aa
jeal <101905536+jealsab@users.noreply.github.com>
2026-03-09 23:35:33 +0300

    Add solution for 3Sum Closest (trial 1)

---
+++ leetcode/3sum-closest_trial1.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        nums.sort()
+        
+        result = sum(nums[:3])
+        for i in range(len(nums)-2):
+            if i==0 and nums[i] == nums[i-1]:
+                continue
+            
+            l, r = i+1, len(nums)-1
+            while l < r:
+                curr = nums[i] + nums[l] + nums[r]
+                diff = curr - target
+                if diff < 0:
+                    l += 1
+                elif diff > 0:
+                    r -= 1
+                else:
+                    return curr
+                """
+                TLE for block if statement
+                
+                if abs(result-target) > abs(diff):
+                    result = curr
+                """
+                result = curr if abs(result-target) > abs(diff) else result
+
+        
+        return result
